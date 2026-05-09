d70931741053ff03123df8324640e339bf485c91
Nitin Dave <nitindave2111@gmail.com>
2026-05-09 18:57:57 +0530

    fix(job): add server default timestamp for updated_at column

--- app/db/models/job.py
+++ app/db/models/job.py
@@ -15,7 +15,7 @@ class Job(Base):
     salary_range = Column(String,nullable=False)
     
     created_at = Column(DateTime(timezone=True),server_default=func.now(),nullable=False)
-    updated_at = Column(DateTime(timezone=True),onupdate=func.now(),nullable=False)
+    updated_at = Column(DateTime(timezone=True),server_default=func.now(),onupdate=func.now(),nullable=False)
     
     employer = relationship("Employer",back_populates="jobs")
     job_status = Column(Enum(JobStatusEnum),default=JobStatusEnum.open)
