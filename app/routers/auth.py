5f4903c5ac06883bbe747bf0418d45e2437e3ba1
Nitin Dave <nitindave2111@gmail.com>
2026-05-09 22:58:21 +0530

    refactor(auth): replace OAuth2PasswordRequestForm with UserLogin schema for login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, Depends, Response, status, HTTPException
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
-from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..import schemas
 
 from app.db.database import get_db
@@ -13,14 +12,14 @@ router = APIRouter(
     tags=['Authentication']
 )
 
-@router.post("/login",response_model=schemas.Token)
-async def login(user_credentials : OAuth2PasswordRequestForm = Depends(),db : Session = Depends(get_db)):
-    user = db.query(user_model.User).filter(user_model.User.user_name == user_credentials.username).first()
+@router.post("/login", response_model=schemas.Token)
+async def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+    user = db.query(user_model.User).filter(user_model.User.user_name == user_credentials.user_name).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Invalid Credentials")
-    if not utils.verify(user_credentials.password,user.user_password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Invalid Credentials")
-    
-    access_token = oauth2.create_access_token(data={"user_id":user.user_id})
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+    if not utils.verify(user_credentials.user_password, user.user_password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
-    return {"access_token":access_token,"token_type":"bearer"}
+    access_token = oauth2.create_access_token(data={"user_id": user.user_id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
