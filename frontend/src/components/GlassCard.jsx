0646401206f743b6d49ba5d0a939e872bc94ef26
Nitin Dave <nitindave2111@gmail.com>
2026-05-11 22:36:23 +0530

    Glassmorphic card with hover effects

---
+++ frontend/src/components/GlassCard.jsx
@@ -0,0 +1,13 @@
+
+const GlassCard = ({ children, className = '', ...props }) => {
+  return (
+    <div
+      className={`glass-card ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GlassCard;
